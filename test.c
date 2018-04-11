8000f768aeee09d74e53615949a4238d1a17dcb3
ManAboutTown <leinaere@gmail.com>
2018-04-11 18:55:40 +0900

    Update test.c

--- test.c
+++ test.c
@@ -23,7 +23,8 @@ void Lottery(int arr[][2]);
 int main(int argc, char *argv[]){
 
     int choice = 0;
-    int arr[6][2] = {{0,0},{0,3},{2,6},{4,4},{6,5},{8,2}};
+    //int arr[6][2] = {{0,0},{0,3},{2,6},{4,4},{6,5},{8,2}};
+    int arr[6][2] = {{0,0},{0,3},{0,6},{0,4},{0,5},{0,2}};
     
         printf("1. FCFS\n");
         printf("2. SJF\n");
@@ -74,7 +75,6 @@ void FCFS(int arr[][2])
     int ready = 0;
     char output[6][20];
 
-
     for (i = 1; i < 6; i++) {
         loop += arr[i][1];
         //printf("%d %d\n", arr[i][0], arr[i][1]);
@@ -97,7 +97,7 @@ void FCFS(int arr[][2])
             arr[ready][1]--; // 프로세스의 실행시간을 1 줄이고
             output[ready][i] = 1; // 아웃풋 데이터에 입력
         }
-    }fflush(stdout);
+    }
 
     for (i = 1; i < 6; i++) {
         for (l = 0; l < loop; l++)  {
@@ -111,7 +111,6 @@ void FCFS(int arr[][2])
     }
 }
 
-
 void SJF(int arr[][2])
 {
     int i,j,k,l;
@@ -121,9 +120,7 @@ void SJF(int arr[][2])
 
     for (i = 1; i < 6; i++) {
         loop += arr[i][1];
-        //printf("%d %d\n", arr[i][0], arr[i][1]);
     }
-    //printf("%d",loop);
 
     for (i = 0; i < loop; i++) {
         ready = 0;
@@ -132,7 +129,7 @@ void SJF(int arr[][2])
                 if (ready == 0) { // 레디에 데이터가 들어갈 수 있는지 판단
                     ready = j; // 레디에 데이터 삽입
                 }
-                else if (arr[j][1] < arr[ready][1]) {
+                else if (arr[j][1] < arr[ready][1]) { // 새로 발견한 프로세스가 더 짧은 실행시간을 가지고 있다면 교체하여 실행
                     ready = j;
                 }
             }
@@ -166,9 +163,7 @@ void RR(int arr[][2])
 
     for (i = 1; i < 6; i++) {
         loop += arr[i][1];
-        //printf("%d %d\n", arr[i][0], arr[i][1]);
     }
-    //printf("%d",loop);
 
     for (i = 0; i < loop; i++) {
         
@@ -233,7 +228,7 @@ void MLFQ(int arr[][2])
                 q[k]++;
             }
             else if (i >= arr[k][0] && k == ready) // 스케쥴링이 직전에 되었다면
-                //q[k] = 0; // 배고픔은 사라짐
+                // 배고픔은 사라짐
                 q[k] = 0;
         }
         
@@ -271,5 +266,73 @@ void MLFQ(int arr[][2])
 }
 void Lottery(int arr[][2])
 {
-    
+    int i,j,k,l;
+    int loop = 0; 
+    int ready = 0;
+    char output[6][20];
+    int ticket = 500; // 현재 사용자가 가진 티켓 수
+    int take_tic[6] = {0,10,40,30,20,50}; // 각 프로세스가 가진 스케쥴링 우선도 티켓 case 1 : 프로세스가 가진 티켓이 사용자가 가진 티켓 미만인 경우
+    int sumtake = 0;
+    //int take_tic2[6] = {0,500,600,200,10,700,400}; // 각 프로세스가 가진 스케쥴링 우선도 티켓 case 1 : 프로세스가 가진 티켓이 사용자가 가진 티켓 미만인 경우
+    int scpr[6] = {0};
+    int gacha = 0;
+    // {{0,0},{0,3},{2,6},{4,4},{6,5},{8,2}} 프로세스 목록
+
+    srand((unsigned int)time(NULL));
+
+    for (i = 1; i < 6; i++) {
+        sumtake += take_tic[i];
+    }
+    //printf("%d\n",sumtake);
+
+    for (i = 1; i < 6; i++) {
+        scpr[i] = scpr[i-1] + ((float)take_tic[i] / sumtake + 0.005) * 100;
+        //printf("%d ",scpr[i]);
+    }
+    scpr[5] += 1;
+    //printf("\n");
+    //printf(" %d",scpr[5]);
+
+    for (i = 1; i < 6; i++) {
+        loop += arr[i][1];
+    }// 총 걸리는 시간 계산
+
+    for (i = 0; i < loop; i++) {
+        for (j = 1; j < 6; j++) {
+            if (gacha == 0) {
+                gacha = rand() % scpr[5];
+                //printf("%d\n",gacha);
+            }
+            if (gacha >= scpr[j-1] && gacha < scpr[j]) {
+                if (i >=arr[j][0] && arr[j][1] >= 1) {
+                    ready = j;
+                }
+                else if (i >=arr[j][0] && arr[j][1] < 1) {
+                    gacha = rand() % scpr[5];
+                    j = 1;
+                    ready = 0;
+                }
+            }
+        }
+
+        if (ready > 0) { // 레디에 처리할 프로세스가 존재한다면
+            arr[ready][1]--; // 프로세스의 실행시간을 1 줄이고
+            output[ready][i] = 1; // 아웃풋 데이터에 입력
+            printf("%d ",gacha);
+            gacha = 0;
+        }
+
+    }
+    printf("\n");
+    for (i = 1; i < 6; i++) {
+        for (l = 0; l < loop; l++)  {
+            if (output[i][l] == 1) {
+                printf(" ■ ");
+            }
+            else
+                printf(" □ ");
+        }
+        printf("\n");
+    }
+
 }
